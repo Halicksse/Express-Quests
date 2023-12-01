7b4d4d8ee477197decf1c6dd5952c097f809fe3b
Halicksse <alix.carlhant.pro@gmail.com>
2023-12-01 10:53:59 +0100

    tests joi

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 
+const validateMovie = require("./middlewares/validateMovie");
+const validateUser = require("./middlewares/validateUser");
+
 const app = express();
 app.use(express.json());
 
@@ -13,10 +16,10 @@ const userControllers = require("./controllers/userControllers");
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUsersById);
 
-app.post("/api/movies", movieControllers.postMovie);
-app.post("/api/users", userControllers.postUsers);
+app.post("/api/movies", validateMovie, movieControllers.postMovie);
+app.post("/api/users", validateUser, userControllers.postUsers);
 
-app.put("/api/movies/:id", movieControllers.updateMovie);
-app.put("/api/users/:id", userControllers.updateUser);
+app.put("/api/movies/:id", validateMovie, movieControllers.updateMovie);
+app.put("/api/users/:id", validateUser, userControllers.updateUser);
 
 module.exports = app;
--- tests/users.test.js
+++ tests/users.test.js
@@ -72,7 +72,7 @@ describe("POST /api/users", () => {
       .post("/api/users")
       .send(userWithMissingProps);
 
-    expect(response.status).toEqual(201);
+    expect(response.status).toEqual(422);
   });
 });
 
@@ -81,7 +81,7 @@ describe("PUT /api/users/:id", () => {
     const newUser = {
       firstname: "Penelope",
       lastname: "Cruz",
-      email:`${crypto.randomUUID()}@aol.com`,
+      email: `${crypto.randomUUID()}@aol.com`,
       city: "Barcelona",
       language: "spanish",
     };
@@ -102,7 +102,7 @@ describe("PUT /api/users/:id", () => {
     const updatedUser = {
       firstname: "Roger",
       lastname: "Rabbit",
-      email:`${crypto.randomUUID()}@aol.com`,
+      email: `${crypto.randomUUID()}@aol.com`,
       city: "Los Angeles",
       language: "english",
     };
@@ -141,7 +141,7 @@ describe("PUT /api/users/:id", () => {
       .put(`/api/users/1`)
       .send(userWithMissingProps);
 
-    expect(response.status).toEqual(500);
+    expect(response.status).toEqual(422);
   });
 
   it("should return no user", async () => {
@@ -154,7 +154,6 @@ describe("PUT /api/users/:id", () => {
     };
 
     const response = await request(app).put("/api/users/0").send(newUser);
-
 
     expect(response.status).toEqual(404);
   });
